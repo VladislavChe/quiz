242d7bd6d90925855a088f3ba63d395b2a880152
Черемисин Владислав <CheremisinVD@intech.rshb.ru>
2024-03-22 15:49:58 +0300

    add Register form

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,56 +1,19 @@
 import React from 'react';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
 import styles from './AuthPage.module.scss'
-import './antd-form.css'
+import LoginForm from "./LoginForm/LoginForm";
+import RegisterForm from "./RegisterForm/RegisterForm";
 
 const AuthPage:React.FC = () => {
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
-    };
+    const [loginForm, setLoginForm] = React.useState<boolean>(true)
 
     return (
         <div className={styles.authPage}>
-            <div className={styles.authCard}>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your Password!' }]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-
-                        <a className="login-form-forgot" href="">
-                            Forgot password
-                        </a>
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                        </Button>
-                        Or <a href="">register now!</a>
-                    </Form.Item>
-                </Form>
+            <div id="components-form-demo-normal-login" className={styles.authCard}>
+                {
+                    loginForm ?
+                        <LoginForm setLoginForm={setLoginForm} /> :
+                        <RegisterForm setLoginForm={setLoginForm} />
+                }
             </div>
         </div>
     );
